e653f0ea2407a11b296c8b6ad8b271e678e4abd5
NANDO <NANDO@DESKTOP-JDSF6KI>
2016-04-20 22:00:31 +0700

    MataKuliah #20/4/2016

---
+++ E-Learning/src/Models/MataKuliah.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author NANDO
+ */
+public class MataKuliah implements Serializable{
+    private String namaMK;
+    private String kodeMK;
+    private int sks;
+
+    public MataKuliah(String namaMK, String kodeMK, int sks) {
+        this.namaMK = namaMK;
+        this.kodeMK = kodeMK;
+        this.sks = sks;
+    }
+
+    public String getNamaMK() {
+        return namaMK;
+    }
+
+    public void setNamaMK(String namaMK) {
+        this.namaMK = namaMK;
+    }
+
+    public String getKodeMK() {
+        return kodeMK;
+    }
+
+    public void setKodeMK(String kodeMK) {
+        this.kodeMK = kodeMK;
+    }
+
+    public int getSks() {
+        return sks;
+    }
+
+    public void setSks(int sks) {
+        this.sks = sks;
+    }
+    
+}
